370c2f0583d622015a7de1e13f61334ececb1e72
Toast Design <info@toastdesign.nl>
2014-12-30 15:40:28 +0100

    Changed bower setting to example online
    More info on https://gist.github.com/realchaseadams/7369801

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,56 @@
-module.exports = function(grunt) {
+var pkgjson = require('./package.json');
+ 
+var config = {
+  pkg: pkgjson,
+  app: 'src',
+  dist: 'dist'
+}
+ 
+module.exports = function (grunt) {
+ 
+  // Configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';'
-      },
+    config: config,
+    pkg: config.pkg,
+    bower: grunt.file.readJSON('./.bowerrc'),
+    copy: {
       dist: {
-        // the files to concatenate
-        src: ['src/**/*.js'],
-        // the location of the resulting JS file
-        dest: 'dist/<%= pkg.name %>.js'
+       files: [{
+         expand: true,
+         cwd: '<%= config.app %>/_lib/font-awesome',
+         src: 'css/font-awesome.min.css',
+         dest: '<%= config.dist %>'
+       },
+       {
+         expand: true,
+         cwd: '<%= config.app %>/_lib/font-awesome',
+         src: 'fonts/*',
+         dest: '<%= config.dist %>'
+       }]
       }
     },
     uglify: {
       options: {
-        // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.name %> lib - v<%= pkg.version %> -' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
       },
       dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          '<%= config.dist %>/js/lib.min.js': [
+            '<%= bower.directory %>/jquery/jquery.js',
+            '<%= bower.directory %>/underscore/underscore.js',
+            '<%= bower.directory %>/requirejs/require.js',
+          ]
         }
       }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      // define the files to lint
-      files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      // configure JSHint (documented at http://www.jshint.com/docs/)
-      options: {
-          // more options here if you want to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
-    },
+    }
   });
-
+ 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-}
+ 
+  grunt.registerTask('default', [
+    'copy',
+    'uglify'
+  ]);
+};
