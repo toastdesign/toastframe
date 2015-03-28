f69dd9e6b0b4a06a9e50f3a85670dbb9322e3adf
Toast Design <info@toastdesign.nl>
2015-03-28 15:26:53 +0100

    Changes to options-framework

--- options.php
+++ options.php
@@ -18,53 +18,6 @@ function optionsframework_option_name() {
 
 function optionsframework_options() {
 
-	// Test data
-	$test_array = array(
-		'one' => __( 'One', 'theme-textdomain' ),
-		'two' => __( 'Two', 'theme-textdomain' ),
-		'three' => __( 'Three', 'theme-textdomain' ),
-		'four' => __( 'Four', 'theme-textdomain' ),
-		'five' => __( 'Five', 'theme-textdomain' )
-	);
-
-	// Multicheck Array
-	$multicheck_array = array(
-		'one' => __( 'French Toast', 'theme-textdomain' ),
-		'two' => __( 'Pancake', 'theme-textdomain' ),
-		'three' => __( 'Omelette', 'theme-textdomain' ),
-		'four' => __( 'Crepe', 'theme-textdomain' ),
-		'five' => __( 'Waffle', 'theme-textdomain' )
-	);
-
-	// Multicheck Defaults
-	$multicheck_defaults = array(
-		'one' => '1',
-		'five' => '1'
-	);
-
-	// Background Defaults
-	$background_defaults = array(
-		'color' => '',
-		'image' => '',
-		'repeat' => 'repeat',
-		'position' => 'top center',
-		'attachment'=>'scroll' );
-
-	// Typography Defaults
-	$typography_defaults = array(
-		'size' => '15px',
-		'face' => 'georgia',
-		'style' => 'bold',
-		'color' => '#bada55' );
-
-	// Typography Options
-	$typography_options = array(
-		'sizes' => array( '6','12','14','16','20' ),
-		'faces' => array( 'Helvetica Neue' => 'Helvetica Neue','Arial' => 'Arial' ),
-		'styles' => array( 'normal' => 'Normal','bold' => 'Bold' ),
-		'color' => false
-	);
-
 	// Pull all the categories into an array
 	$options_categories = array();
 	$options_categories_obj = get_categories();
@@ -79,7 +32,6 @@ function optionsframework_options() {
 		$options_tags[$tag->term_id] = $tag->name;
 	}
 
-
 	// Pull all the pages into an array
 	$options_pages = array();
 	$options_pages_obj = get_pages( 'sort_column=post_parent,menu_order' );
@@ -87,9 +39,6 @@ function optionsframework_options() {
 	foreach ($options_pages_obj as $page) {
 		$options_pages[$page->ID] = $page->post_title;
 	}
-
-	// If using image radio buttons, define a directory path
-	$imagepath =  get_template_directory_uri() . '/images/';
 
 	$options = array();
 
