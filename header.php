07d83e7d921c1c7bcf090d72278862ba8b0f7de1
Toast Design <info@toastdesign.nl>
2015-03-29 13:45:42 +0200

    Styling for lists

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 			</div><!-- .site-branding -->
 			
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'h-hor-list h-clean-list', 'container'=> '') ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'h-clean-list', 'container'=> '') ); ?>
 			</nav><!-- #site-navigation -->
 		</div>
 	</header><!-- #masthead -->
