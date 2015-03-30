78977f8159b03f15f71ae6993e2bf688e536ba44
Toast Design <info@toastdesign.nl>
2015-03-30 14:42:05 +0200

    Off canvas navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 (function() {
-  var foo = "hello ",
-      bar = "world!";
-  
-  console.log(foo + bar);
+  	$("body").click(function(){
+    	$(".main-navigation-off-canvas").toggleClass("open");
+	});
 })();
