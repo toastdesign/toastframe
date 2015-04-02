c5cac355e335f48d423aa594b0c31121573c0ffc
Toast Design <info@toastdesign.nl>
2015-04-02 15:29:04 +0200

    Reinstalled busy the good way

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
         dist: {
             files: {
                 'dist/css/master.css': 'scss/master.scss'
-            }
+            },
         }
     },
     watch: {
