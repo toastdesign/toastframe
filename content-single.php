7a82abf4dae7eab311efd035ca4d4ac7ab23f794
Toast Design <info@toastdesign.nl>
2015-08-28 15:20:01 +0200

    Removed comments

--- content-single.php
+++ content-single.php
@@ -1,10 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php toastframe_posted_on(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>	
+	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
--- content.php
+++ content.php
@@ -8,12 +8,6 @@
 	<?php the_post_thumbnail(); ?>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php toastframe_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
